2c9a00cf8f688116216eca2cbc1ad06f17eb2bb9
Emmanuel Kieffer <emmanuel.kieffer@lxp.lu>
2026-01-02 17:33:31 +0100

    Revert config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,17 +1,5 @@
 return {
   'neovim/nvim-lspconfig',
-  init = function()
-    vim.api.nvim_create_autocmd('CursorHold', {
-      callback = function()
-        vim.diagnostic.open_float(nil, {
-          focus = false,
-          scope = 'cursor',
-          border = 'rounded',
-          source = 'if_many',
-        })
-      end,
-    })
-  end,
   opts = {
     diagnostics = {
       virtual_text = false,
