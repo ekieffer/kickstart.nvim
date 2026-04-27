602d188566208b60d0a2695527902d5c8b20763f
Emmanuel Kieffer <emmanuel.kieffer@lxp.lu>
2026-04-27 17:25:27 +0200

    Added new keymaps

--- lua/plugins/keymaps.lua
+++ lua/plugins/keymaps.lua
@@ -2,6 +2,7 @@ return {
   {
     'LazyVim/LazyVim',
     keys = {
+      -- Your existing mapping
       {
         '<leader>wsq',
         function()
@@ -9,6 +10,28 @@ return {
           vim.cmd 'normal! P'
         end,
         desc = 'Word Surround Quotes',
+        mode = 'n',
+      },
+
+      -- Move line down
+      {
+        '<leader>j',
+        function()
+          vim.cmd 'move +1'
+          vim.cmd 'normal! =='
+        end,
+        desc = 'Move line down',
+        mode = 'n',
+      },
+
+      -- Move line up
+      {
+        '<leader>k',
+        function()
+          vim.cmd 'move -2'
+          vim.cmd 'normal! =='
+        end,
+        desc = 'Move line up',
         mode = 'n',
       },
     },
